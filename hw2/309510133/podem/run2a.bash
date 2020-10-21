297c0351986295a351e4278c5b68aafac66ed8e3
leopardshadow <thurtu811@gmail.com>
2020-10-21 22:11:56 +0800

    update hw2 (submit)

---
+++ hw2/309510133/podem/run2a.bash
@@ -0,0 +1,5 @@
+gtime -f "Average memory usage: %K \nMaximum memory usage: %M\n" ./atpg -mod_logicsim -input c17_2.input c17.bench
+gtime -f "Average memory usage: %K \nMaximum memory usage: %M\n" ./atpg -mod_logicsim -input c17_3.input c17.bench
+gtime -f "Average memory usage: %K \nMaximum memory usage: %M\n" ./atpg -mod_logicsim -input c17_4.input c17.bench
+gtime -f "Average memory usage: %K \nMaximum memory usage: %M\n" ./atpg -mod_logicsim -input c17_5.input c17.bench
+
---
+++ hw2/309510133/podem/testc17.bash
@@ -0,0 +1,3 @@
+./atpg -mod_logicsim -input c17o.input c17.bench
+./atpg -logicsim -input c17o.input c17.bench
+
