bb528111e3917d645c870c5cd09a16a05c8b4efa
leopardshadow <thurtu811@gmail.com>
2020-11-23 11:25:47 +0800

    add hw4-b: generates adjacent bridging faults

--- hw4/podem/bridgingFaults.h
+++ hw4/podem/bridgingFaults.h
@@ -1,31 +1,28 @@
 #ifndef BRIDGING_FAULTS_H
 #define BRIDGING_FAULTS_H
+
 #include "gate.h"
+
+enum BFAULT_TYPE {AND, OR};
 
 class BRIDGING_FAULT
 {
     private:
-        // VALUE Value;
-        // GATE* Input;
-        // GATE* Output; //record output gate for branch fault
-        // //if stem, Input = Output
-        // bool Branch; //fault is on branch
-        // unsigned EqvFaultNum; //equivalent fault number (includes itself)
-        // FAULT_STATUS Status;
+        BFAULT_TYPE FType;
+        GATE* gptr1;
+        GATE* gptr2;
     public:
-        // FAULT(GATE* gptr, GATE* ogptr, VALUE value): Value(value), Input(gptr),
-        // Output(ogptr), Branch(false), EqvFaultNum(1), Status(UNKNOWN) {}
-        // ~FAULT() {}
-        // VALUE GetValue() { return Value; }
-        // GATE* GetInputGate() { return Input; }
-        // GATE* GetOutputGate() { return Output; }
-        // void SetBranch(bool b) { Branch = b; }
-        // bool Is_Branch() { return Branch; }
-        // void SetEqvFaultNum(unsigned n) { EqvFaultNum = n; }
-        // void IncEqvFaultNum() { ++EqvFaultNum; }
-        // unsigned GetEqvFaultNum() { return EqvFaultNum; }
-        // void SetStatus(FAULT_STATUS status) { Status = status; }
-        // FAULT_STATUS GetStatus() { return Status; }
+        BRIDGING_FAULT(BFAULT_TYPE FType, GATE* gptr1, GATE* gptr2) {
+            this->FType = FType;
+            this->gptr1 = gptr1;
+            this->gptr2 = gptr2;
+        }
+        string output() {
+            string out = "";
+            out = out + "(" + gptr1->GetName() + ", " + gptr2->GetName() + ", ";
+            out = out + (FType ? "OR" : "AND") + ")";
+            return out;
+        }
 };
 
 
