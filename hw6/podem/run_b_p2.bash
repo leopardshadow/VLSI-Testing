17d6914e1507097d7eeabff6b9a3315504518427
leopardshadow <thurtu811@gmail.com>
2020-12-24 12:19:15 +0800

    update hw6 part b running command

---
+++ hw6/podem/run_b_p2.bash
@@ -0,0 +1,34 @@
+ echo "./atpg -fsim -input b17_1_cp_vectors b17.bench "
+./atpg -fsim -input b17_1_cp_vectors b17.bench 
+
+ echo "./atpg -fsim -input b17_1_vectors b17.bench "
+./atpg -fsim -input b17_1_vectors b17.bench 
+
+ echo "./atpg -fsim -input b17_10_cp_vectors b17.bench "
+./atpg -fsim -input b17_10_cp_vectors b17.bench 
+
+ echo "./atpg -fsim -input b17_10_vectors b17.bench "
+./atpg -fsim -input b17_10_vectors b17.bench 
+
+ echo "./atpg -fsim -input b17_100_cp_vectors b17.bench "
+./atpg -fsim -input b17_100_cp_vectors b17.bench 
+
+ echo "./atpg -fsim -input b17_100_vectors b17.bench "
+./atpg -fsim -input b17_100_vectors b17.bench 
+
+ echo "./atpg -fsim -input b17_1000_cp_vectors b17.bench "
+./atpg -fsim -input b17_1000_cp_vectors b17.bench 
+
+ echo "./atpg -fsim -input b17_1000_vectors b17.bench "
+./atpg -fsim -input b17_1000_vectors b17.bench 
+
+ echo "./atpg -fsim -input b17_cp_vectors b17.bench "
+./atpg -fsim -input b17_cp_vectors b17.bench 
+
+ echo "./atpg -fsim -input b17_vectors b17.bench "
+./atpg -fsim -input b17_vectors b17.bench 
+
+ 
+
+ 
+
